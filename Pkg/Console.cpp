f8efb8c5b2cf291335c7a2cbc8695de7223e6f35
chrisvanlier2005 <87958@glr.nl>
2022-12-08 14:08:09 +0100

    added draw Serperator function

--- Pkg/Console.cpp
+++ Pkg/Console.cpp
@@ -11,3 +11,12 @@ void Console::Write(std::string text) {
 	std::cout << Console::currentColor << text << RESET << std::endl;
 	text.clear();
 }
+
+void Console::Serperator(int length = 10, char character = '-')
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		std::cout << character;
+	}
+	std::cout << '\n';
+}
--- Pkg/Console.h
+++ Pkg/Console.h
@@ -5,6 +5,7 @@ class Console
 public:
 	static void SetColor(std::string colorCode);
 	static void Write(std::string text);
+	static void Serperator(int length = 10, char character = '-');
 private:
 	static std::string currentColor;
 };
